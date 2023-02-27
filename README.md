6df61d919699d49d3a98e956c042c809e1bf9400
Emily Bettencourt <emmicourt@gmail.com>
2023-02-26 16:28:58 -0800

    add lint and prettier for my sanity

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TaskAssertions
+
 A custom azure dev ops task to help test and validate other tasks.
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { run } from "./index";
+
+describe("index", () => {
+  beforeEach(() => {});
+
+  test("input validation should passs", () => {});
+});
