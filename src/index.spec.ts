c68ac8d9f9a03b4dc796c01bb4d8dc36d2353b1f
Emily Bettencourt <emmicourt@gmail.com>
2023-02-26 17:05:40 -0800

    prettier

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,23 +4,24 @@ import tl = require("azure-pipelines-task-lib/task");
 describe("index input validation", () => {
   beforeEach(() => {});
 
-  afterEach(() => {    
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
   test("Task should fail if required inputs not given", () => {
-
     let tlSpy = jest.spyOn(tl, "setResult");
-    run(); 
+    run();
 
-    expect(tlSpy).toHaveBeenCalledWith(tl.TaskResult.Failed, "Input required: samplestring");
+    expect(tlSpy).toHaveBeenCalledWith(
+      tl.TaskResult.Failed,
+      "Input required: samplestring"
+    );
   });
 
   test("Task should succeed if valid input given", () => {
-
     let tlSpy = jest.spyOn(tl, "setResult");
     let tlGetInput = jest.spyOn(tl, "getInput").mockReturnValue(" there.");
-    run(); 
+    run();
 
     expect(tlSpy).toHaveBeenCalledTimes(0);
     expect(tlGetInput).toHaveBeenLastCalledWith("samplestring", true);
