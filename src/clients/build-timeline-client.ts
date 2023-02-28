8d25f1327421dc7c95c55407723507fc966df05b
Emily Bettencourt <emmicourt@gmail.com>
2023-02-28 08:25:47 -0800

    add retries to client

--- src/clients/build-timeline-client.ts
+++ src/clients/build-timeline-client.ts
@@ -14,8 +14,15 @@ export class BuildTimelineClient implements IBuildTimelineClient {
 
   public async getBuildTimeline(
     project: string,
-    buildId: number
+    buildId: number,
+    numRetry = 1
   ): Promise<Timeline> {
-    return await this.buildApi.getBuildTimeline(project, buildId);
+    return this.retry(numRetry, await this.buildApi.getBuildTimeline(project, buildId));
+  }
+
+  private retry(numRetry:number, fn) : unknown{
+    return new Promise(fn).catch(error => numRetry > 0
+      ? this.retry(numRetry - 1, fn)
+      : Promise.reject(error));
   }
 }
